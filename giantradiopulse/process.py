68247e53af92164e72a12865f9a8131beb9c3ecd
Teruaki Enoto <teruakienoto@Teruakis-MacBook.local>
2018-10-27 10:48:07 +0900

    remote

--- giantradiopulse/process.py
+++ giantradiopulse/process.py
@@ -261,7 +261,7 @@ class ProcessManager():
 
 	def run_correlation_study(self,nphase=60,lagrange=2):
 		for obs in self.observationunit_list:			
-			obs.show_parameters()
+			print(obs.outdir)
 		#	print(obs.param)
 			#setup_yaml = '%s/%s/%s_setup.yaml' % (self.outdir,obs.param['dataid'],obs.param['dataid'])
 			#print(setup_yaml)
