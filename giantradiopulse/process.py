f5f612ee8d2a8a64a8d260ca60580b7117a3ac82
Teruaki Enoto <teruakienoto@Teruakis-MacBook.local>
2018-10-27 10:41:18 +0900

    remote

--- giantradiopulse/process.py
+++ giantradiopulse/process.py
@@ -261,9 +261,9 @@ class ProcessManager():
 	def run_correlation_study(self,nphase=60,lagrange=2):
 		for obs in self.observationunit_list:			
 			print(obs.param)
-			setup_yaml = '%s/%s/%s_setup.yaml' % (self.outdir,obs.param['dataid'],obs.param['dataid'])
-			print(setup_yaml)
-			obs.reload_parameter_yamlfile(setup_yaml)
+			#setup_yaml = '%s/%s/%s_setup.yaml' % (self.outdir,obs.param['dataid'],obs.param['dataid'])
+			#print(setup_yaml)
+			#obs.reload_parameter_yamlfile(setup_yaml)
 			#obs.generate_correlation_xrayprofile_fitsfile(
 			#	nphase=self.param['NPHASE'],
 			#	lagrange=self.param['LAGRANGE'],
