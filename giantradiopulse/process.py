964bdf392c48abb51cd99bc97c00c5dc171b46f4
Teruaki Enoto <teruakienoto@Teruakis-MacBook.local>
2018-10-27 10:45:56 +0900

    remote

--- giantradiopulse/process.py
+++ giantradiopulse/process.py
@@ -253,7 +253,7 @@ class ProcessManager():
 				sys.stdout.write('-- ObservationUnit {} is skipped.\n'.format(row['dataid']))
 				continue 
 			self.observationunit_list.append(ObservationUnit(row,self.param,self.outdir))			
-			self.observationunit_list[-1].show_parameters()
+			#self.observationunit_list[-1].show_parameters()
 
 	def prepare_datafiles(self):
 		for obs in self.observationunit_list:
